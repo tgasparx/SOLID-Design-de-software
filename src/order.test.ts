77e7fb6ef1d093d4184cbbe1f4f24611429cb271
Thiago <gasparsthiago@gmail.com>
2022-06-07 00:32:57 -0300

    inicio- instanciando classe

--- src/order.test.ts
+++ src/order.test.ts
@@ -1,3 +1,8 @@
-test("Order Test", function () {
+/** **************************** INICIO
+ *  *****Puxando classe externa, o New Order está dando erro, pois espera algo que não existe.
+ ******** Se é novo, há um antigo, há um pai.   */
 
-});
+function tesaat<Promise>() {
+  const order = new Order();
+}
+
